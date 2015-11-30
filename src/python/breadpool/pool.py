03c78e1a51b3ac8f78dfcb81e400f1314c253a36
Chamila de Alwis <chamilad@wso2.com>
2015-12-01 00:37:06 +0530

    Initial commit

---
+++ src/python/breadpool/pool.py
@@ -0,0 +1,132 @@
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+
+from Queue import Queue, Empty
+from threading import Thread
+import traceback
+import logging
+
+log = logging.getLogger(__name__)
+log.addHandler(logging.NullHandler)
+
+
+class WorkerThread(Thread):
+    def __init__(self, task_queue, thread_name, polling_timeout):
+        super(WorkerThread, self).__init__(name=thread_name)
+        self.__task_queue = task_queue
+        """ :type : Queue """
+        self.__terminated = False
+        self.__thread_name = thread_name
+        self.__polling_timeout = polling_timeout
+
+    def run(self):
+        while not self.__terminated:
+            # locking implemented by the Queue class
+            # blocks until a task is queued
+            task = None
+            try:
+                task = self.__task_queue.get(timeout=self.__polling_timeout)
+            except Empty:
+                pass
+
+            if task is not None:
+                log.info("Work!")
+                # print "Work!!"
+                try:
+                    task.execute()
+                except Exception as ex:
+                    log.info("Caught exception while executing task : %s" % ex.message)
+                    # print "Caught exception while executing task : %s" % ex.message
+                    traceback.print_exc()
+
+                    # notify the queue that the task is done
+                self.__task_queue.task_done()
+
+        log.debug("Terminating worker thread %s" % self.__thread_name)
+        # print "Terminating worker thread %s" % self.__thread_name
+
+    def terminate(self):
+        self.__terminated = True
+
+
+class ThreadPool(object):
+    def __init__(self, size, name, daemon=False, polling_timeout=60):
+        if size == 0:
+            raise ValueError("Thread pool size should be more than 0")
+
+        self.__task_queue = Queue()
+        self.__pool_size = size
+        self.__pool_name = name
+        self.__worker_threads = []
+        self.__polling_timeout = polling_timeout
+        self.__daemon = daemon
+        self.create_worker_threads()
+
+    def enqueue(self, task):
+        # thread safety comes from Queue class
+        self.__task_queue.put(task)
+
+    def get_pool_size(self):
+        return self.__pool_size
+
+    def terminate(self):
+        log.debug("Waiting until all the tasks are done")
+        # print "Waiting until all the tasks are done"
+        self.__task_queue.join()
+        log.debug("Sending termination signal for the worker threads")
+        # print "Sending termination signal for the worker threads"
+        for worker_thread in self.__worker_threads:
+            worker_thread.terminate()
+
+    def create_worker_threads(self):
+        while len(self.__worker_threads) < self.__pool_size:
+            thread_name = "%s-%s" % (self.__pool_name, (len(self.__worker_threads) + 1))
+            worker_thread = WorkerThread(self.__task_queue, thread_name, self.__polling_timeout)
+            worker_thread.setDaemon(self.__daemon)
+            worker_thread.start()
+            self.__worker_threads.append(worker_thread)
+            # print "Worker thread created %s" % thread_name
+
+
+class ScheduledJobExecutor(Thread):
+    def __init__(self, task, thread_pool, delay):
+        super(ScheduledJobExecutor, self).__init__()
+        self.__task = task
+        """ :type : AbstractScheduledTask """
+        self.__thread_pool = thread_pool
+        """ :type : ThreadPool  """
+        self.__delay = delay
+        self.__terminated = False
+
+    def run(self):
+        # start job
+        raise NotImplementedError
+
+    def terminate(self):
+        self.__terminated = True
+
+
+class AbstractScheduledTask(object):
+    """
+    Extend AbstractScheduledTask to implement the task to be scheduled.
+    """
+
+    def __init__(self):
+        raise NotImplementedError
+
+    def execute(self):
+        raise NotImplementedError
---
+++ src/python/breadpool/tests/test_pool.py
@@ -0,0 +1,80 @@
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+
+import pytest
+import threading
+import datetime
+import time
+from ..pool import *
+
+"""
+ThreadPool
+"""
+
+
+def test_thread_pool_value_exception():
+    with pytest.raises(ValueError):
+        ThreadPool(0, "TestThreadPoolValException")
+
+
+def test_thread_pool_value_property():
+    thread_pool = ThreadPool(5, "TestThreadPoolValueProperty", polling_timeout=1)
+    assert thread_pool.get_pool_size() == 5
+    thread_pool.terminate()
+
+
+def test_thread_pool_pool_thread_size():
+    thread_pool = ThreadPool(5, "TestThreadPoolSize", polling_timeout=1)
+    live_threads = get_threads_with_name("TestThreadPoolSize")
+
+    assert len(live_threads.keys()) == 5
+    thread_pool.terminate()
+
+
+def test_thread_pool_polling_timeout():
+    thread_pool = ThreadPool(5, "TestThreadPoolPolling", polling_timeout=5)
+    before_time = datetime.datetime.now()
+    # print before_time.time()
+    thread_pool.terminate()
+    while len(get_threads_with_name("TestThreadPoolPolling").keys()) > 4:
+        time.sleep(0.5)
+
+    after_time = datetime.datetime.now()
+    # print after_time.time()
+    diff_time = after_time - before_time
+    print diff_time.seconds
+    # assert False
+    assert diff_time.seconds == 5
+
+
+def test_thread_pool_daemon_flag():
+    thread_pool = ThreadPool(2, "TestThreadPoolDaemonFlag", daemon=True, polling_timeout=1)
+    created_threads = get_threads_with_name("TestThreadPoolDaemonFlag")
+    # print len(created_threads)
+    thread_name, thread_obj = created_threads.popitem()
+    assert thread_obj.daemon is True
+    thread_pool.terminate()
+
+
+def get_threads_with_name(thread_name):
+    live_threads = threading.enumerate()
+    named_threads = {}
+    for live_thread in live_threads:
+        if thread_name in live_thread.name:
+            named_threads[live_thread.name] = live_thread
+
+    return named_threads
